e39b6dcddb9b44a49ed4dd9af358b84da9c41b78
calvinkranig <calvinkranig@gmail.com>
2021-10-20 18:05:46 -0400

    solved leet code Container With Most Water

---
+++ Medium/watercontainer.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+      int l = 0;
+      int r = height.size()-1;
+      int maxSize = 0;
+      
+      while(l<r) {
+        maxSize = max((r-l) * min(height[l],height[r]),maxSize);
+        if(height[l]<height[r]) {
+          l++;
+        } else {
+          r--;
+        }
+      }
+      return maxSize;
+    }
+};
